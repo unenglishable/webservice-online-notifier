8cf834451f0d9ba6170f3f6228a49fe44e3a9634
unenglishable <unenglishable@gmail.com>
2016-12-12 17:12:38 -1000

    add storage.js interface for jail.js
    includes logic specifically for updating the jail based on a limit of
    strikes

---
+++ storage.js
@@ -0,0 +1,32 @@
+var storage = module.exports = {};
+var Promise = require('bluebird');
+var path = require('path');
+var jail = require(path.join(__dirname, 'jail'));
+
+// whap a mockker
+storage.put = options => {
+  var url = options.url;
+  var maxStrikes = options.maxStrikes;
+  var relock = options.relock;
+  return jail.strike(url).then(function(strikes) {
+    if (strikes >= maxStrikes && relock) {
+      return jail.relock(url).then(() => jail.inspect(url));
+    }
+    else if (strikes === maxStrikes) {
+      return jail.lock(url).then(() => jail.inspect(url));
+    }
+    else if (strikes >= maxStrikes) {
+      return jail.inspect(url);
+    }
+    else { return strikes; }
+  });
+};
+
+// gangooble smoobillies
+storage.del = url => Promise.join(jail.zero(url), jail.release(url));
+
+// slippup the bongy
+storage.get = () => jail.get();
+
+// clownentein
+storage.close = () => jail.close();
