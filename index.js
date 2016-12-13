f8d9501da5341d077c6b840d55cd279d53cea48c
unenglishable <unenglishable@gmail.com>
2016-12-12 17:20:35 -1000

    use storage.put in index (on fail)
    index does a storage.put when failing.  if the stored date is less than
    the current date, notify and reset the date!

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var storage = require(path.join(__dirname, 'storage'));
 var urls = config.urls;
 var keyword = config.keyword;
 var postCheckHook = config.postCheckHook;
+var maxStrikes = config.maxStrikes;
 
 tester(urls, keyword)
 .each(function(result) {
@@ -15,12 +16,20 @@ tester(urls, keyword)
     return storage.del(result.url);
   }
   else if (result.status === 'offline') {
-    result.errors.forEach(function(error) {
-      console.log(result.url, 'is offline:');
+    console.log(result.url, 'is offline!');
+    return storage.put({ url: result.url, maxStrikes })
+    .then(function(jailtime) {
+      console.log(jailtime, '<?', Date.now());
+      if (jailtime < Date.now()) {
+        // notify and relock
+        return storage.put({ url: result.url, maxStrikes, relock: true})
+        .then(function() {
+          if (postCheckHook) {
+            request.post({ url: postCheckHook, body: { text: result.url + ' is offline: ' + JSON.stringify(result.errors) }, json: true });
+          }
+        });
+      }
     });
-    if (postCheckHook) {
-      request.post({ url: postCheckHook, body: { text: result.url + ' is offline: ' + JSON.stringify(result.errors) }, json: true });
-    }
   }
   else {
     console.log('¯\\_(ツ)_/¯');
