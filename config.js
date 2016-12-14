b109715705490a30b7cf690208fc648b20910b00
unenglishable <unenglishable@gmail.com>
2016-12-13 15:34:37 -1000

    config defaults, number coercion, env url array

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 require('dotenv').load({ silent: true });
-var configJSON = require('config.json');
+var configJSON, envUrls, path = require('path');
+try { configJSON = require(path.join(__dirname, 'config.json')); }
+catch (error) { configJSON = {}; }
+if (process.env.URLS) { envUrls = process.env.URLS.split(' '); }
 module.exports = {
-  postCheckHook: process.env.POST_CHECK_HOOK || configJSON.postCheckHook,
-  keyword: process.env.KEYWORD || configJSON.keyword,
-  maxStrikes: process.env.MAX_STRIKES || configJSON.maxStrikes,
-  urls: configJSON.urls
+  postCheckHook: process.env.POST_CHECK_HOOK || configJSON.postCheckHook || '',
+  keyword: process.env.KEYWORD || configJSON.keyword || '',
+  maxStrikes: Number(process.env.MAX_STRIKES) || Number(configJSON.maxStrikes) || 5,
+  urls: envUrls || configJSON.urls || []
 };
