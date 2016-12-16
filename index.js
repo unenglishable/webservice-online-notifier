31eb907f3ddb2beaac62792aacafd556c536ed3f
unenglishable <unenglishable@gmail.com>
2016-12-16 11:11:35 -1000

    index exports a function
    can now be executed as a function (for running in another script)

--- index.js
+++ index.js
@@ -9,31 +9,30 @@ var keyword = config.keyword;
 var postCheckHook = config.postCheckHook;
 var maxStrikes = config.maxStrikes;
 
-tester(urls, keyword)
-.each(function(result) {
-  if (result.status === 'online') {
-    console.log(result.url, 'is online!');
-    return storage.del(result.url);
-  }
-  else if (result.status === 'offline') {
-    console.log(result.url, 'is offline!');
-    return storage.put({ url: result.url, maxStrikes })
-    .then(function(jailtime) {
-      if (jailtime > maxStrikes && jailtime <= Date.now()) {
-        // notify and relock
-        return storage.put({ url: result.url, maxStrikes, relock: true})
-        .then(function() {
-          if (postCheckHook) {
-            request.post({ url: postCheckHook, body: { text: result.url + ' is offline: ' + JSON.stringify(result.errors) }, json: true });
-          }
-        });
-      }
-    });
-  }
-  else {
-    console.log('¯\\_(ツ)_/¯');
-  }
-})
-.then(function() {
-  return storage.close();
-});
+module.exports = function() {
+  return tester(urls, keyword)
+  .each(function(result) {
+    if (result.status === 'online') {
+      console.log(result.url, 'is online!');
+      return storage.del(result.url);
+    }
+    else if (result.status === 'offline') {
+      console.log(result.url, 'is offline!');
+      return storage.put({ url: result.url, maxStrikes })
+      .then(function(jailtime) {
+        if (jailtime > maxStrikes && jailtime <= Date.now()) {
+          // notify and relock
+          return storage.put({ url: result.url, maxStrikes, relock: true})
+          .then(function() {
+            if (postCheckHook) {
+              request.post({ url: postCheckHook, body: { text: result.url + ' is offline: ' + JSON.stringify(result.errors) }, json: true });
+            }
+          });
+        }
+      });
+    }
+    else {
+      console.log('¯\\_(ツ)_/¯');
+    }
+  });
+};
