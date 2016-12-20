2d70b67e7fe5fad1ea6003753f355f8c33d16f2a
unenglishable <unenglishable@gmail.com>
2016-12-19 14:14:27 -1000

    add emails to config.js

--- config.js
+++ config.js
@@ -3,11 +3,13 @@ var configJSON, envUrls, path = require('path');
 try { configJSON = require(path.join(__dirname, 'config.json')); }
 catch (error) { configJSON = {}; }
 if (process.env.URLS) { envUrls = process.env.URLS.split(' '); }
+if (process.env.EMAILS) { envEmails = process.env.EMAILS.split(' '); }
 module.exports = {
   postCheckHook: process.env.POST_CHECK_HOOK || configJSON.postCheckHook || '',
   keyword: process.env.KEYWORD || configJSON.keyword || '',
   maxStrikes: Number(process.env.MAX_STRIKES) || Number(configJSON.maxStrikes) || 5,
   urls: envUrls || configJSON.urls || [],
+  emails: envEmails || configJSON.emails || [],
   redis: {
     host: process.env.REDIS_PORT_6379_TCP_ADDR || process.env.REDIS_HOST,
     port: process.env.REDIS_PORT_6379_TCP_PORT || process.env.REDIS_PORT,
